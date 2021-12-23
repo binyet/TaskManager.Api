b858b9b8a12fa866e2c48efbadc4acea7dd4bf1d
binyet <1244257841@qq.com>
2021-12-23 23:19:47 +0800

    添加加密解密工具

---
+++ Encryption.Tools/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaskManager.Utility;
+
+namespace Encryption.Tools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string key = "binyet1234567890binyet1234567890";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Encryp_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_decryp.Text.Trim()))
+            {
+                return;
+            }
+            tb_encryp.Text = AESEncrypHelper.AesEncrypt(tb_decryp.Text, key);
+        }
+
+        private void Decryp_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_encryp.Text.Trim()))
+            {
+                return;
+            }
+            tb_decryp.Text = AESEncrypHelper.AesDecrypt(tb_encryp.Text, key);
+        }
+
+    }
+}
---
+++ TaskManager.Utility/AESEncrypHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TaskManager.Utility
+{
+    public static class AESEncrypHelper
+    {
+
+        /// <summary>
+        ///  AES 加密
+        /// </summary>
+        /// <param name="str">明文（待加密）</param>
+        /// <param name="key">密文</param>
+        /// <returns></returns>
+        public static string AesEncrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            Byte[] toEncryptArray = Encoding.UTF8.GetBytes(str);
+
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.None
+            };
+
+            ICryptoTransform cTransform = rm.CreateEncryptor();
+            Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        /// <summary>
+        ///  AES 解密
+        /// </summary>
+        /// <param name="str">明文（待解密）</param>
+        /// <param name="key">密文</param>
+        /// <returns></returns>
+        public static string AesDecrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            Byte[] toEncryptArray = Convert.FromBase64String(str);
+
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.None
+            };
+
+            ICryptoTransform cTransform = rm.CreateDecryptor();
+            Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
+
+        public static (bool, int) GetTest()
+        {
+            int a = 10;
+            bool b = true;
+            return (b, a);
+        }
+    }
+}
